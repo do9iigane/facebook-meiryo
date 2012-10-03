0d5660f6ae8db958644b26168fdc780c0019ba65
毒眼鏡 <>
2012-10-03 19:53:06 +0900

    facebookが"meiryo"を含めるように修正したので、あえて"ms gothic"で表示するように改悪

--- source/font-change.js
+++ source/font-change.js
@@ -1,5 +1,10 @@
-$('.home').css('font-family','meiryo');
-$('.inputpassword').css('font-family','meiryo');
-$('.inputtext').css('font-family','meiryo');
-$('textarea').css('font-family','meiryo');
+font = '"msp gothic"';
+$('.home').css('font-family',font);
+$('.inputpassword').css('font-family',font);
+$('.inputtext').css('font-family',font);
+$('.uiButtonText').css('font-family',font);
+$('.uiButton').css('font-family',font);
+$('textarea').css('font-family',font);
+$('body').css('font-family',font);
+$('input').css('font-family',font);
 //alert('loaded');
